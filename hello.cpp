6f60df203c428c85fbc5a5e621dabb3e9842423f
EuclideanDreamer <78236576+EuclideanDreamer@users.noreply.github.com>
2021-01-29 14:55:10 -0800

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,21 @@
+//* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+//* Program name	: hello.cpp	* 
+//*	* 
+//* Written by	: Bailey Nichols	* 
+//*	* 
+//* Purpose	: This program prints 'Hello World.	* 
+//*	* 
+//*---------------------------------------------------------------------* 
+//* Change Log:	* 
+//*	Revision	* 
+//*	Date	Changed  Rel Ver Mod Purpose	* 
+//* 01/29/21	Nichols 001.000.000 Initial release of program	* 
+//*	* 
+//* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+#include <iostream>
+using namespace std;
+int main()
+{
+    printf("Hello World\n");
+    return 0;
+}
